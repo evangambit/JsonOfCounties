668225586c101a25ab3fa9b09b0e5fd23700c71a
Thomas Redding <thomasredding@Thomass-MacBook-Pro-3.local>
2020-06-24 12:36:11 -0500

    Improve how we compute county area.

--- create_json.py
+++ create_json.py
@@ -1,4 +1,4 @@
-import csv, json, os, re
+import csv, json, math, os, re
 
 # pip install pyshp
 import shapefile
@@ -154,12 +154,14 @@ for s in sf:
 		states[state] = {}
 	county_name = s.record.NAMELSAD.lower()
 	poly = geometry.Polygon(s.shape.points)
-
 	states[state][county_name] = {
 		"area": poly.area,
 		"min_location": poly.bounds[:2],
 		"max_location": poly.bounds[2:]
 	}
+	latitude_ish = (states[state][county_name]["min_location"][1] + states[state][county_name]["max_location"][1]) / 2
+	states[state][county_name]["area"] = math.cos(latitude_ish)
+
 
 def add_demographics(states):
 	age_code_to_group = {
